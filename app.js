9e58c51dda4c1a1682048530e577fc352cf094be
Jacob Saporito <japorito@gmail.com>
2017-03-02 21:50:12 -0800

    Fix messed up login URL to use configured value.

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use('/',
         },
         index);
 app.use('/admin',
-        ensureLoggedIn('/login'),
+        ensureLoggedIn(config.googleCredentials.callbackURL),
         admin);
 
 // catch 404 and forward to error handler
@@ -98,7 +98,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = !config.production ? err : {};
 
   // render the error page
   res.status(err.status || 500);
